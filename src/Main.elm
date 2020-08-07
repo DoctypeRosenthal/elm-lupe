23e3c02aa511897a3c05f2c6668ee971a89694a0
Lorenz Rosenthal <lorenz.rosenthal@cbc.de>
2020-08-07 22:52:00 +0200

    feat: implement a css-grid based solution so ToDos can scale together with ticks

--- src/Main.elm
+++ src/Main.elm
@@ -3,12 +3,30 @@ module Main exposing (Model, Msg(..), dayStartsAt, init, main, minuteGranularity
 import Browser
 import Browser.Dom exposing (Element)
 import Html exposing (Html, div)
-import Html.Attributes exposing (class, style)
+import Html.Attributes exposing (class, classList, style)
+import Html.Events exposing (onClick)
 import Html.Events.Extra.Pointer as Pointer exposing (Event)
+import List.Extra as List
 
 
 
 ---- MODEL ----
+
+
+type alias Todo =
+    { startTick : TickIndex
+    , endTick : TickIndex
+    , title : String
+    , backgroundColor : String
+    }
+
+
+defaultTodo =
+    { startTick = 7 * 12
+    , endTick = 8 * 12
+    , title = "bla"
+    , backgroundColor = "rgb(155,0,155)"
+    }
 
 
 type alias TickIndex =
@@ -20,6 +38,7 @@ type alias Model =
     , allTicks : List Element
     , activeTick : Maybe TickIndex
     , offsetTop : Float
+    , todos : List Todo
     }
 
 
@@ -29,6 +48,7 @@ init =
       , allTicks = []
       , offsetTop = 0
       , activeTick = Nothing
+      , todos = [ defaultTodo ]
       }
     , Cmd.none
     )
@@ -41,6 +61,7 @@ init =
 type Msg
     = SetTimelineOffset Float
     | SetActiveTick (Maybe Int)
+    | ClickOnActiveTick
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -51,6 +72,25 @@ update msg model =
 
         SetActiveTick tickIndex ->
             ( { model | activeTick = tickIndex }, Cmd.none )
+
+        ClickOnActiveTick ->
+            let
+                activeTick =
+                    Maybe.withDefault 0 model.activeTick
+
+                maybeTodo =
+                    List.find (\x -> x.startTick <= activeTick && activeTick <= x.endTick) model.todos
+
+                _ =
+                    Debug.log "click on active Tick" <|
+                        case maybeTodo of
+                            Just aTodo ->
+                                "Click auf ToDo " ++ aTodo.title
+
+                            Nothing ->
+                                ""
+            in
+            ( model, Cmd.none )
 
 
 
@@ -78,11 +118,11 @@ ticksPerHour =
 
 
 numberOfTicks =
-    wokeHours * ticksPerHour
+    wokeHours * ticksPerHour + 1
 
 
 ticksArr =
-    List.range 0 (wokeHours * ticksPerHour + 1)
+    List.range 0 numberOfTicks
 
 
 g : Float -> Float
@@ -95,12 +135,12 @@ g number =
 
 
 f : Int -> Int -> Float
-f x activeTick =
+f activeTick x =
     -0.00001 * toFloat (x - activeTick) ^ 4 + 1
 
 
-tickView : Maybe Int -> Int -> Html Msg
-tickView activeTick index =
+tickView : Int -> Float -> Html Msg
+tickView index scalar =
     let
         myTime =
             (toFloat index * minuteGranularity) / minutesPerHour + dayStartsAt
@@ -115,14 +155,6 @@ tickView activeTick index =
             else
                 [ div [ class "tick__number" ] [ Html.text <| String.fromFloat myTime ]
                 ]
-
-        scalar =
-            case activeTick of
-                Just tickIndex ->
-                    (g << f index) tickIndex
-
-                Nothing ->
-                    0
 
         tickLineCssValue =
             String.fromFloat
@@ -140,23 +172,67 @@ tickView activeTick index =
                 , style "transform" <| "scaleX(" ++ tickLineCssValue ++ ")"
                 ]
                 []
-
-        tickHeight =
-            String.fromFloat <| 100.0 / numberOfTicks + scalar * 0.4
     in
     div
         [ Html.Attributes.class "tick"
-        , style "min-height" <| tickHeight ++ "%"
         , Pointer.onOver (always (Just index) >> SetActiveTick)
+        , onClick ClickOnActiveTick
         ]
         (tickNumberView ++ [ tickLine ])
 
 
-timelineView : Maybe Int -> Html Msg
-timelineView activeTick =
+todoView : TickIndex -> Todo -> Html Msg
+todoView activeTick { startTick, endTick, backgroundColor, title } =
     div
-        [ Html.Attributes.id "timeline" ]
-        (List.map (tickView activeTick) ticksArr)
+        [ classList [ ( "todo", True ), ( "todo--hover", startTick <= activeTick && activeTick <= endTick ) ]
+        , style "grid-row" (String.fromInt (startTick + 1) ++ " / " ++ String.fromInt (endTick + 1))
+        , style "background" backgroundColor
+        ]
+        [ Html.text title ]
+
+
+timelineView : Model -> Html Msg
+timelineView model =
+    let
+        scaledTicks =
+            case model.activeTick of
+                Just activeTickIndex ->
+                    List.map (g << f activeTickIndex) ticksArr
+
+                Nothing ->
+                    List.map (always 0) ticksArr
+
+        tickHeights : List Float
+        tickHeights =
+            List.map (\scalar -> 100.0 / numberOfTicks + scalar) scaledTicks
+
+        renderedTicks =
+            List.indexedMap tickView scaledTicks
+
+        separator =
+            div
+                [ class "timeline__separator"
+                , style "grid-row" ("1 / " ++ String.fromInt numberOfTicks)
+                ]
+                []
+
+        todosView =
+            let
+                tickIndex =
+                    case model.activeTick of
+                        Just activeTickIndex ->
+                            activeTickIndex
+
+                        Nothing ->
+                            0
+            in
+            List.map (todoView tickIndex) model.todos
+    in
+    div
+        [ class "timeline"
+        , style "grid-template-rows" (String.join " " <| List.map (\x -> String.fromFloat x ++ "%") tickHeights)
+        ]
+        (renderedTicks ++ [ separator ] ++ todosView)
 
 
 view : Model -> Html Msg
@@ -165,15 +241,11 @@ view model =
         [ class "App"
         , Pointer.onOut (always Nothing >> SetActiveTick)
         ]
-        [ div [ class "timeline__filter" ] [ timelineView model.activeTick ]
+        [ div [ class "timeline__filter" ] [ timelineView model ]
         ]
 
 
 
---
---relativePos : Pointer.Event -> ( Float, Float )
---relativePos event =
---    event.pointer.offsetPos
 ---- PROGRAM ----
 
 
